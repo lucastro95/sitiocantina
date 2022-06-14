d62843fa5c8771209955ae66619973eac4d684b0
Lucía Castro <castrolucia2108@gmail.com>
2022-06-14 20:02:32 -0300

    un monton de cambios

---
+++ js/reservas.js
@@ -0,0 +1,145 @@
+// Constantes
+const formulario = document.getElementById("form");
+const inputs = document.querySelectorAll("#form .inputs");
+const boton = document.querySelector('#boton-submit')
+
+// Variables
+let nombre = false
+let apellido = false
+let telefono = false
+let email = false
+let sucursal = false
+let cantidad = false
+let fecha = false
+
+
+// Expresiones regulares
+const expresiones = {
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+};
+
+
+// Validación de formulario
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "nombre":
+      // Comprobar si el usuario ingresado es válido
+      if (expresiones.nombre.test(e.target.value)) {
+        document.getElementById("error-nombre").style = "visibility:hidden";
+        nombre = true
+      } else {
+        document.getElementById("error-nombre").style = "visibility:visible";
+        nombre = false
+      }
+      break;
+
+    case "apellido":
+      if (expresiones.nombre.test(e.target.value)) {
+        document.getElementById("error-apellido").style = "visibility:hidden";
+        apellido = true
+      } else {
+        document.getElementById("error-apellido").style = "visibility:visible";
+        apellido = false
+      }
+      break;
+
+    case "telefono":
+      if (expresiones.telefono.test(e.target.value)) {
+        document.getElementById("error-telefono").style = "visibility:hidden";
+        telefono = true
+      } else {
+        document.getElementById("error-telefono").style = "visibility:visible";
+        telefono = false
+      }
+      break;
+
+    case "email":
+      if (expresiones.correo.test(e.target.value)) {
+        document.getElementById("error-email").style = "visibility:hidden";
+        email = true
+      } else {
+        document.getElementById("error-email").style = "visibility:visible";
+        email = false
+      }
+      break;
+
+    case "sucursal":
+      if (e.target.value != "") {
+        document.getElementById("error-sucursal").style = "visibility:hidden";
+        sucursal = true
+      } else {
+        document.getElementById("error-sucursal").style = "visibility:visible";
+        sucursal = false
+      }
+      break;
+
+    case "cantidad":
+      if (e.target.value != "") {
+        document.getElementById("error-cant").style = "visibility:hidden";
+        cantidad = true
+      } else {
+        document.getElementById("error-cant").style = "visibility:visible";
+        cantidad = false
+      }
+      break;
+
+    case "fecha":
+      if (e.target.value) {
+        document.getElementById("error-cant").style = "visibility:hidden";
+        fecha = true
+      } else {
+        document.getElementById("error-cant").style = "visibility:visible";
+        fecha = false
+      }
+      break;
+
+    default:
+      break;
+  }
+
+  if (apellido && nombre && email && telefono && sucursal && cantidad && fecha) {
+    boton.disabled = false
+  } else {
+    boton.disabled = true
+  }
+};
+
+/* const validarSelect = (e) => {
+  switch (e.target.name) {
+    case "sucursal":
+      if (e.target.value != "") {
+        document.getElementById("error-sucursal").style = "visibility:hidden";
+        boton.disabled = false
+      } else {
+        document.getElementById("error-sucursal").style = "visibility:visible";
+        boton.disabled = true
+      }
+      break;
+
+    case "cantidad":
+      if (e.target.value != "") {
+        document.getElementById("error-cant").style = "visibility:hidden";
+        boton.disabled = false
+      } else {
+        document.getElementById("error-cant").style = "visibility:visible";
+        boton.disabled = true
+      }
+      break;
+  }
+}; */
+
+
+// Event listeners
+inputs.forEach((input) => {
+  input.addEventListener("blur", validarFormulario);
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("change", validarFormulario);
+});
+
+// fecha.addEventListener('change', validarFecha) 
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault()
+});
